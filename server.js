a71fd56f2ba905090bc5f34014ca5ebd81cafacd
Sandy <sandy.jernigan@gmail.com>
2019-08-10 00:04:36 -0400

    Separate the endpoints that begin with `/api/posts` into a separate `Express Router`.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const HubsRouter = require('./posts/posts-router.js');
 
 const server = express();
 server.use(express.json());
+
+// Separate the endpoints that begin with `/api/posts` into a separate `Express Router`.
 server.use('/api/posts', PostsRouter);
 
 server.get('/', (req, res) => {
