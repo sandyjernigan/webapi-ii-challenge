c2b583603e09723f1073afa07b709aed48255a2b
Sandy <sandy.jernigan@gmail.com>
2019-08-11 17:36:07 -0400

    Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -83,4 +83,26 @@ router.post('/:id/comments', async (req, res) => {
   }
 });
 
+// Update
+// 	Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+router.put('/:id', async (req, res) => {
+  console.log(req.params.id)
+  try {
+    // update(id, post): accepts two arguments, the first is the id of the post to update and the second is an object with the changes to apply. It returns the count of updated records. If the count is 1 it means the record was updated correctly.
+    const updateResults = await DB.update(req.params.id, req.body);
+    if (updateResults) {
+      const results = await DB.findById(req.params.id);
+      res.status(200).json(results);
+    } else {
+      res.status(404).json({ message: 'The post could not be found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the post',
+    });
+  }
+});
+
 module.exports = router;
